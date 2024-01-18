1dbbf1e9cb12914db5b3a31eb7016e562ef4054f
Vỏ Phú Cường <phucuong.vo@catthaicorp.com>
2024-01-18 22:33:28 +0700

    token

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require('express')
-const { GenerateJWT } = require('./services/jwt.service')
+const { GenerateJWT, VerifyJWT, verifyAccessToken, signAccessToken } = require('./services/jwt.service')
+require('dotenv').config()
+
 const app = express()
+app.use(express.json())
+
+const SECRET_KEY = process.env.SECRET_KEY
 
 app.get('/', async (req, res) => {
-    const token = await GenerateJWT({ name: 'cuongvp 123'}, 'phu cuong')
-    console.log(token)
+
+    const data = {
+        name: 'cuongvp 123'
+    }
+
+    const token = await signAccessToken(data)
+    res.header('Authorization', 'Bearer '+ token)
     res.send(token)
+})
+
+app.post('/verify', async (req, res) => {
+
+    if(!req.headers['authorization']){
+        return res.send({
+            error: 'do not have token'
+        }) 
+    }
+    
+    const authHeader = req.headers['authorization']
+    const bearerToken = authHeader.split(' ')
+    const token = bearerToken[1]
+   
+    if(token){
+        const decode = await verifyAccessToken(token)
+        return res.send({
+            data: decode
+        })
+    }
 })
 
 
--- services/jwt.service.js
+++ services/jwt.service.js
@@ -1,11 +1,63 @@
 const jwt = require('jsonwebtoken')
+require('dotenv')
 
-const GenerateJWT = async (payload, sig) => {
-    var token = jwt.sign(payload, sig);
+const GenerateJWT = async (payload, sig, options) => {
+    var token = jwt.sign(payload, sig, options);
     return token
+}
+
+const VerifyJWT = async (token, key) => {
+    const decode = jwt.verify(token, key, (err, decoded) => {
+        if(err){
+            return err
+        }
+        return decoded
+    })
+    return decode
+}
+
+const signAccessToken = async (payload) => {
+    const options = {
+        expiresIn: '1m'
+    }
+    var token = jwt.sign(payload, process.env.ACCESS_TOKEN, options)
+    return token
+}
+ 
+const signRefreshToken = async (payload) => {
+    const options = {
+        expiresIn: '5m'
+    }
+    var token = jwt.sign(payload, process.env.REFRESH_TOKEN, options)
+    return token
+}
+
+const verifyAccessToken = async (token) => {
+    const decode = jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if(err){
+            return err
+        }
+        return decoded
+    })
+    return decode
+}
+
+const verifyRefressToken = async (token) => {
+    const decode = jwt.verify(token, process.env.REFRESH_TOKEN, (err, decoded) => {
+        if(err){
+            return err
+        }
+        return decoded
+    })
+    return decode
 }
 
 
 module.exports = {
-    GenerateJWT
+    GenerateJWT,
+    VerifyJWT,
+    signAccessToken,
+    signRefreshToken,
+    verifyAccessToken,
+    verifyRefressToken
 }
